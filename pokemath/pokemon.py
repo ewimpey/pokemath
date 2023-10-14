27a64ee75f8d43e1ddbc65bcf9a3554b380f04c2
ewimpey <42553063+ewimpey@users.noreply.github.com>
2023-10-14 11:45:11 -0400

    Update pokemon.py for image paths

--- pokemath/pokemon.py
+++ pokemath/pokemon.py
@@ -8,7 +8,9 @@ class Pokemon:
         self.health = health
         self.original_health = health
         self.type_ = type_
-        self.image_path = pkg_resources.resource_filename('pokemath', image_path)
+        adjusted_image_path = '../' + image_path 
+        self.image_path = pkg_resources.resource_filename('pokemath', adjusted_image_path)
+        #self.image_path = pkg_resources.resource_filename('pokemath', image_path)
         self.possible_moves = possible_moves
         self.equipped_moves = []
         
