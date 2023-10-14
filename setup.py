32df018709849b8d7dc81811afb189a66aa5159c
ewimpey <42553063+ewimpey@users.noreply.github.com>
2023-10-14 11:56:41 -0400

    Update setup.py to find pictures

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+import glob
+
+# Gather all .png files from the img directory
+image_files = glob.glob('img/*.png')
 
 setup(
     name="pokemath",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
+    data_files=[('img', image_files)],
     install_requires=[
        "beautifulsoup4==4.12.2",
        "pypokedex==1.6.0",
